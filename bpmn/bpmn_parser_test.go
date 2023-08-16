1b0f20154cdf318a51a24125ccd7e5159222b7db
lack <598223084@qq.com>
2023-08-16 15:56:50 +0800

    feat: bpmn schema

---
+++ bpmn/bpmn_parser_test.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2021 Aree Enterprises, Inc. and Contributors
+// Use of this software is governed by the Business Source License
+// included in the file LICENSE
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/LICENSE-Apache-2.0
+
+package bpmn
+
+import (
+	"encoding/xml"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseSample(t *testing.T) {
+	var sampleDoc Definitions
+	var err error
+	sample, err := testdata.ReadFile("testdata/sample.bpmn")
+	if err != nil {
+		t.Fatalf("Can't read file: %v", err)
+	}
+	err = xml.Unmarshal(sample, &sampleDoc)
+	if err != nil {
+		t.Fatalf("XML unmarshalling error: %v", err)
+	}
+	processes := sampleDoc.Processes()
+	assert.Equal(t, 1, len(*processes))
+
+	out, err := xml.MarshalIndent(sampleDoc, "", " ")
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	t.Log(string(out))
+}
+
+func TestParseSampleNs(t *testing.T) {
+	var sampleDoc Definitions
+	var err error
+	sampleNs, err := testdata.ReadFile("testdata/sample_ns.bpmn")
+	if err != nil {
+		t.Fatalf("Can't read file: %v", err)
+	}
+	err = xml.Unmarshal(sampleNs, &sampleDoc)
+	if err != nil {
+		t.Fatalf("XML unmarshalling error: %v", err)
+	}
+	processes := sampleDoc.Processes()
+	assert.Equal(t, 1, len(*processes))
+}
