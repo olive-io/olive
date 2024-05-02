d94ec0b2cca71115d92adda936f3df5f9461e2be
lack <598223084@qq.com>
2024-05-02 21:16:02 +0800

    modify

--- console/config/config.go
+++ console/config/config.go
@@ -26,10 +26,11 @@ import (
 	"net/http"
 	urlpkg "net/url"
 
+	"github.com/spf13/pflag"
+
 	"github.com/olive-io/olive/client"
 	"github.com/olive-io/olive/pkg/cliutil/flags"
 	"github.com/olive-io/olive/pkg/logutil"
-	"github.com/spf13/pflag"
 )
 
 const (
